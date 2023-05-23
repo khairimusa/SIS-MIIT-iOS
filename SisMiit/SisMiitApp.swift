40facd8b0a985f24a6395beac942accc58a6b077
khairimusa <mohammadkhairibinmu@airasia.com>
2023-05-23 11:12:03 +0800

    Initial Commit

---
+++ SisMiit/SisMiitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SisMiitApp.swift
+//  SisMiit
+//
+//  Created by Khairi Musa on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SisMiitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
